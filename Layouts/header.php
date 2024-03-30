059e258cd4405a9dc0f83810a1237bcea60dd322
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-03-30 14:31:51 +0100

    feat(alkebulan_layout): #3 set up header project

---
+++ Layouts/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" initial-scale=1.0>
+    <title>ALKEBULAN</title>
+    <link rel="stylesheet" href="../CSS/global.css">
+    <link rel="stylesheet" href="../CSS/header.css">
+</head>
+
+<body>
+    <header>
+        <img src="../Images/logo_alkebulan.png" alt="logo alkebulan" class="logo_img" />
+        <nav>
+            <ul class="link_element">
+                <li><a href="" class="active">Accueil</a></li>
+                <li><a href=""> Forum</a></li>
+                <li><a href=""> Quiz</a></li>
+                <li><a href=""> Boutique</a></li>
+            </ul>
+            <ul class="link_authenticate">
+                <li><a href=""><img src="../Images/icon_for_user.png" alt="user icon" class="user_icon" />S'inscrire</a>
+                </li>
+                <li><a href="">Se connecter</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
